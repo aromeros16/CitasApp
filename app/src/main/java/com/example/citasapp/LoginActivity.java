74e994f0058d19c343186ddf2500f57b333ebcb4
aromeros16 <Torrente16#>
2020-05-22 11:30:12 +0200

    Intento desarrollo login

--- app/src/main/java/com/example/citasapp/LoginActivity.java
+++ app/src/main/java/com/example/citasapp/LoginActivity.java
@@ -4,17 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //Ocultar actionbar
+        getSupportActionBar().hide();
+
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
